30dbbc9cfbae6dd540af2406b339e950aa455f90
Umidjon <umidjonshodiyorov@gmail.com>
2022-06-13 23:27:03 -0700

    navbar

--- src/components/Carusel/index.jsx
+++ src/components/Carusel/index.jsx
@@ -1,8 +1,135 @@
-import React, { useEffect } from "react";
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+import Img1 from '../../assets/img/12.jpg';
+import Img2 from '../../assets/img/13.jpg';
+import Img3 from '../../assets/img/14.jpg';
 
-export default () => {
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:Img1,
 	
-	return (
-		<>a</>
-	);
-};
+  },
+  {
+    label: 'Bird',
+    imgPath:Img2,
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:Img3,
+  },
+
+];
+
+function SwipeableTextMobileStepper() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+//   const handleStepChange = (step: number) => {
+//     setActiveStep(step);
+//   };
+
+  return (
+    <Box sx={{ maxWidth: "100%", flexGrow: 1, paddingBottom:'250px' }}>
+      {/* <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 50,
+          pl: 2,
+          bgcolor: 'background.default',
+        }}
+      >
+        <Typography>{images[activeStep].label}</Typography>
+      </Paper> */}
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        // onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 594,
+                  display: 'block',
+                  maxWidth: "100%",
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+			<Box sx={{textAlign:'center',color:'white',position:"absolute",marginTop:'-400px',width:'100%'}} >
+				<Typography sx={{letterSpacing:'2px'}} >Women Collection 2018</Typography>
+				<Typography sx={{fontWeight:'bold',fontSize:'70px',letterSpacing:'5px'}} >NEW ARRIVALS</Typography>
+				<Button variant='contained' sx={{paddingLeft:'3%',background:'white',color:'black','&:hover':{ background:'red',color:'white' },paddingTop:'0.7%',paddingBottom:'0.7%',borderRadius:'30px',paddingRight:'3%',}} >SHOP NOW</Button>
+
+			</Box>
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+	  sx={{marginTop:'-200px'}}
+        // steps={maxSteps}
+        position="static"
+        // activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+			sx={{marginTop:'-180px'}}
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+			>
+            
+            {theme.direction === 'rtl' ? (
+				<KeyboardArrowLeft  />
+				) : (
+					<KeyboardArrowRight />
+					)}
+          </Button>
+        }
+        backButton={
+			<Button sx={{marginTop:'-180px'}} size="small" onClick={handleBack} disabled={activeStep === 0}>
+				
+            {theme.direction === 'rtl' ? (
+				<KeyboardArrowRight />
+				) : (
+					<KeyboardArrowLeft />
+					)}
+            
+          </Button>
+        }
+      />
+    </Box>
+  );
+}
+
+export default SwipeableTextMobileStepper;
--- src/components/image_list/index.js
+++ src/components/image_list/index.js
@@ -1,66 +1,7 @@
-import * as React from 'react';
-import "./style.css"
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Picture_one from "../../assets/img/1.webp"
-import Picture_two from "../../assets/img/3.webp"
-import Picture_thre from "../../assets/img/5.webp"
-import Picture_four from "../../assets/img/2.webp"
-import Picture_five from "../../assets/img/4.webp"
-import Picture_six from "../../assets/img/6.jpg"
+import React from "react";
 
-const Item = styled(Paper)(({ theme }) => ({
-	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-	...theme.typography.body2,
-	padding: theme.spacing(1),
-	textAlign: 'center',
-	color: theme.palette.text.secondary,
-}));
-
-export default function RowAndColumnSpacing() {
+export default function MasonryImageList() {
 	return (
-		<Box sx={{ width: '100%' }}>
-			<Grid justifyContent="center" container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-				<Grid item xs={9} sm={9} md={3.5} lg={3.5}>
-					<Box className='zoom' sx={{ textAlign: 'center' }} >
-						<img className='img1' src={Picture_one} />
-						<Button sx={{ marginTop: '-40%', "&:hover": { background: '#e65540', color: 'white' }, transition: "0.4s", color: '#333', fontSize: '16px', borderRadius: '0px', background: '#fffffff2', paddingLeft: '15%', paddingRight: '15%', paddingTop: '3%', paddingBottom: '3%' }} >Dresses</Button>
-					</Box>
-				</Grid>
-				<Grid item xs={9} sm={9} md={3.5} lg={3.5}>
-					<Box className='zoom1' sx={{ textAlign: 'center', height: { xs: '100%', sm: '100%', md: '67%', lg: '67%' } }} >
-						<img className='img1' src={Picture_two} />
-						<Button sx={{ marginTop: '-40%', "&:hover": { background: '#e65540', color: 'white' }, transition: "0.4s", color: '#333', fontSize: '16px', borderRadius: '0px', background: '#fffffff2', paddingLeft: '15%', paddingRight: '15%', paddingTop: '3%', paddingBottom: '3%' }} >WATCHES</Button>
-					</Box>
-				</Grid>
-				<Grid sx={{ marginTop: { xs: '0%', sm: '0%', md: '0', lg: '0' } }} item xs={9} sm={9} md={3.5} lg={3.5}>
-					<Box className='zoom' sx={{ textAlign: 'center' }} >
-						<img className='img1' src={Picture_five} />
-						<Button sx={{ marginTop: '-40%', "&:hover": { background: '#e65540', color: 'white' }, transition: "0.4s", color: '#333', fontSize: '16px', borderRadius: '0px', background: '#fffffff2', paddingLeft: '15%', paddingRight: '15%', paddingTop: '3%', paddingBottom: '3%' }} >BAGS</Button>
-					</Box>
-				</Grid>
-				<Grid item xs={9} sm={9} md={3.5} lg={3.5}>
-					<Box className='zoom' sx={{ textAlign: 'center' }} >
-						<img className='img1' src={Picture_four} />
-						<Button sx={{ marginTop: '-40%', "&:hover": { background: '#e65540', color: 'white' }, transition: "0.4s", color: '#333', fontSize: '16px', borderRadius: '0px', background: '#fffffff2', paddingLeft: '15%', paddingRight: '15%', paddingTop: '3%', paddingBottom: '3%' }} >SUNGLASSES</Button>
-					</Box>
-				</Grid>
-				<Grid itemxs={9} sm={9} md={3.5} lg={3.5}>
-					<Box className='zoom' sx={{ textAlign: 'center', width: { xs: '73%', sm: '97%', md: '94%', lg: '94%' }, marginLeft: { xs: '14.3%', sm: '2.5%', md: '6%', lg: '6%' }, marginTop: { xs: "0", sm: '0', md: '-34%', lg: '-34%' }, height: { xs: '96%', sm: '96%', md: '132%', lg: '132%' } }} >
-						<img className='img1' src={Picture_thre} />
-						<Button sx={{ marginTop: '-40%', "&:hover": { background: '#e65540', color: 'white' }, transition: "0.4s", color: '#333', fontSize: '16px', borderRadius: '0px', background: '#fffffff2', paddingLeft: '15%', paddingRight: '15%', paddingTop: '3%', paddingBottom: '3%' }} >FOOTERWEAR</Button>
-					</Box>
-				</Grid>
-				<Grid item xs={9} sm={9} md={3.5} lg={3.5}>
-					<Box className='zoom' sx={{ textAlign: 'center' }} >
-						<img className='img1' src={Picture_six} />
-						<Button sx={{ marginTop: '-40%', "&:hover": { background: '#e65540', color: 'white' }, transition: "0.4s", color: '#333', fontSize: '16px', borderRadius: '0px', background: '#fffffff2', paddingLeft: '15%', paddingRight: '15%', paddingTop: '3%', paddingBottom: '3%' }} >ACCESSORIES</Button>
-					</Box>
-				</Grid>
-			</Grid>
-		</Box>
+		<>a</>
 	);
 }
